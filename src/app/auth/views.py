0be3067bf542f9f335abe61b67a810e8ba9b059a
david-jerry <jeremiahedavid@gmail.com>
2024-09-24 12:43:23 +0200

    fixed auth api error

--- src/app/auth/views.py
+++ src/app/auth/views.py
@@ -279,13 +279,14 @@ Please check your email, a new verification code has been sent to you
             expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
         )
 
-        user: UserRead = await get_current_active_user()
+        user: User = await get_current_active_user()
+        user_data = UserRead(**user.model_dump())
 
         return {
             "message": "Login successful",
             "access_token": access_token,
             "refresh_token": refresh_token,
-            "user": user,
+            "user": user_data,
         }
 
 
